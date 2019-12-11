6e8c8cc09ef7c14556a42f0f13808c784d070a0e
vladyslavnUA <vladyslavn@yahoo.com>
2019-12-11 01:04:51 -0800

    added options

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 # cryptokey is a random password generator
+import random
+
+characters = 'abcdefghijklmnopqrstuvwxyz1234567890'
+
+def generate():
+    select = input("how many passwords do you want to generate? ")
+    length = input("length of password(s)? ")
+
+    
