36afdafbed7c40c311b6c321c5c5d1e03af08a25
vladyslavnUA <vladyslavn@yahoo.com>
2019-12-11 11:15:18 -0800

    wrote main function

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ import random
 
 characters = 'abcdefghijklmnopqrstuvwxyz1234567890'
 
-def generate():
-    select = input("how many passwords do you want to generate? ")
-    length = input("length of password(s)? ")
+select = input("how many passwords do you want to generate? ")
+select = int(select)
+length = input("length of password(s)? ")
+length = int(length)
 
-    
+for i in range(select):
+    for c in range(length):
+        password = ''
+        password += random.choice(characters)
+        print(password, end = '')
+    print(select)
