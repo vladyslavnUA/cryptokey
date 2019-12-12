cc5085b748e410898319e27988ac42af754da7bc
vladyslavnUA <vladyslavn@yahoo.com>
2019-12-12 14:02:26 -0800

    updated functions

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
 # cryptokey is a random password generator
 import random
+from random import choice
 
-characters = 'abcdefghijklmnopqrstuvwxyz1234567890'
-
-select = input("how many passwords do you want to generate? ")
-select = int(select)
-length = input("length of password(s)? ")
-length = int(length)
+characters = 'abcdefghijklmnopqrstuvwxyz'
+uppercase_chars = characters.upper()
+numbers = '0123456789'
+symbols = '!@#$%&*()[]{}'
 
 for i in range(select):
-    for c in range(length):
-        password = ''
-        password += random.choice(characters)
+    password = ''
+    for b in range(length):
+        chars = random.choice(characters) + random.choice(uppercase_chars) + random.choice(numbers) + random.choice(symbols)
+        password += (chars)
         print(password, end = '')
     print(select)
+
+
+#def password():
+    #pchars = random.choice(characters)
+    #psymbols = random.choice(symbols)
+    #pnumbers = random.choice(numbers)
+    #pupper = random.choice(uppercase_chars)
+    #crypto_password = str(pchars) + str(psymbols) + str(pnumbers) + str(pupper)
+    #return crypto_password
+
+charss = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*'
+AmountOfPasswords = int(input('how many passwords do you want to generate? '))
+lengthh = int(input('length of password(s)? '))
+
+for p in range(AmountOfPasswords):
+    passwordd = ''
+    for c in range(0, lengthh):
+        passwordd += random.choice(charss)
+    print(passwordd)
---
+++ cryptopass.py
@@ -0,0 +1,26 @@
+import string
+import time
+from random import choice
+
+start_time = time.time()
+
+def createPassword():
+    password = ""
+    
+    while True:
+        answer = int(input("Press #1 to Select Difficult Password, #2 for Easy Password: "))
+        if answer == 1:
+            # Difficult Password
+            allchar = string.ascii_letters + string.punctuation + string.digits
+            password = "".join(choice(allchar) for x in range(12))
+            break
+        elif answer == 2:
+            # Easy Password
+            allchar = string.ascii_letters + string.digits
+            password = "".join(choice(allchar) for i in range(8))
+            break
+    print(password)
+
+createPassword()
+print("--- %s run-time code in seconds ---" % round((time.time() - start_time),2))
+ 
